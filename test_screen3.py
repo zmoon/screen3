2ca32229f522d75d996e1015e20a4d0a7fe9db2d
zmoon92 <zmoon92@gmail.com>
2021-04-08 19:41:53 -0400

    Add test for src dir with spaces

--- test_screen3.py
+++ test_screen3.py
@@ -1,10 +1,14 @@
+import platform
+from pathlib import Path
+
+import pytest
+
 import screen3
 
 
 def test_examples_run():
     import importlib
     import sys
-    from pathlib import Path
 
     examples = Path(__file__).parent / "examples"
     assert examples.is_dir()
@@ -12,3 +16,24 @@ def test_examples_run():
 
     for p in examples.glob("*.py"):
         importlib.import_module(p.stem)
+
+
+@pytest.mark.skipif(platform.system() != "Windows", reason="On Windows, we don't have to build")
+def test_run_src_dir_with_spaces():
+    import shutil
+    import warnings
+
+    import urllib3
+
+    p = Path(__file__).parent / "src dir with spaces"
+    p.mkdir(exist_ok=True)
+
+    with pytest.warns(urllib3.exceptions.InsecureRequestWarning):
+        screen3.download(extract_to=p)
+
+    exe0 = screen3._SCREEN_EXE_PATH
+    screen3.set_screen_exe_path(p / "SCREEN3.EXE")
+    screen3.run_screen()
+    screen3.set_screen_exe_path(exe0)
+
+    shutil.rmtree(p)
